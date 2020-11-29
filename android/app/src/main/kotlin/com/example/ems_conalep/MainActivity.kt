999a9e4166124edee670eee5f80cd2bf2f61a6b1
themaxhs <themaxhs@gmail.com>
2020-11-28 23:29:39 -0500

    splash and login

---
+++ android/app/src/main/kotlin/com/example/ems_conalep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ems_conalep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+import 'package:ems_conalep/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  theme:
+  ThemeData(
+    primaryColor: Colors.lightGreen, accentColor: Colors.white),
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+));
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+
+    super.initState();
+    Timer(Duration(seconds: 3),
+      ()=>Navigator.pushReplacement(
+        context,
+					MaterialPageRoute(
+            builder:
+						(context) => 
+						  LoginPage()
+						)
+			)
+		);
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.greenAccent
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 50.0,
+                      child: Icon(
+                        Icons.school,
+                        color: Colors.blueAccent,
+                        size: 50.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                    ),
+                    Text(
+                      "EMS Conalep",
+                      style: TextStyle(
+                        color: Colors.white, 
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold
+                        ),
+                    )
+                  ],
+                ),
+              )
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 20.0
+                    ),
+                  ),
+                  Text(
+                    "Waiting...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                    )
+                ],
+              ),
+            )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(title:Text("EMS Conalep")),
+	body: Center(
+		child:Text("EMS Conalep",textScaleFactor: 2,)
+	),
+	);
+}
+}
